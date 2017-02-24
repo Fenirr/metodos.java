0ebf42ef6735dae782e4aa4e73676be6521f6b50
Fenirr <162t0305@itsm.edu.mx>
2017-02-24 11:02:39 -0600

    sin retorno con parametros
    ejemplos de metodos en  java

---
+++ ejemplo2.5.java
@@ -0,0 +1,13 @@
+package metodo2;
+
+public class ejemplo5 {
+public static void main(String[]args){
+		
+	    resta(584,45);
+	}
+		public static void resta(int s,int p){
+			int resta= (s - p);
+			System.out.println("la resta es :"+resta);
+
+}
+}
